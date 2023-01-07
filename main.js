8429be7e515affcfa0fbb2b1c32652ef308fe34c
MarioFlores <mfvcreative@gmail.com>
2023-01-07 14:39:16 -0800

    Merged with dev

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-const gameEngine = new GameEngine();
-
-const ASSET_MANAGER = new AssetManager();
-
-ASSET_MANAGER.downloadAll(() => {
-	const canvas = document.getElementById("gameWorld");
-	const ctx = canvas.getContext("2d");
-
-	gameEngine.init(ctx);
-
-	gameEngine.start();
-});
 
 const gameEngine = new GameEngine();
 
