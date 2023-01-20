e51e6e9e667b3e02e061b7612679f2b7bd4f6513
Andrew Nguyen <andrewnguyen841@gmail.com>
2023-01-20 00:00:43 -0800

    move animation class to const notation

--- src/utils/Animation.js
+++ src/utils/Animation.js
@@ -4,14 +4,14 @@
  * @author Andrew Nguyen
  * @version 1/19/23
  */
-class Animation {
-    /**
-     * Initializes Animation object.
-     * @param {number} firstFrameX X position of first frame (not pixel position!)
-     * @param {number} lastFrameX  X position of last frame (not pixel position!), firstFrameX by default
-     * @param {number} frameY      Y position of start frame (not pixel position!)
-     */
-    constructor({firstFrameX, lastFrameX = firstFrameX, frameY }) {
-        Object.assign(this, { firstFrameX, lastFrameX, frameY });
-    };
-}
+
+/**
+ * Initializes Animation object.
+ * @param {number} firstFrameX X position of first frame (not pixel position!)
+ * @param {number} lastFrameX  X position of last frame (not pixel position!), firstFrameX by default
+ * @param {number} frameY      Y position of start frame (not pixel position!)
+ */
+const Animation = function({firstFrameX, lastFrameX = firstFrameX, frameY }) {
+    Object.assign(this, { firstFrameX, lastFrameX, frameY });
+};
+Animation.prototype.name = 'animation';
