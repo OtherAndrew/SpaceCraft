4ae9bc1453cd46f7ff88b32bdd7c14c1b919c3f0
Andrew Nguyen <andrewnguyen841@gmail.com>
2023-01-28 00:48:17 -0800

    convert NPC.js to ES6 format

--- src/entities/NPC.js
+++ src/entities/NPC.js
@@ -5,28 +5,33 @@
  * @version 1/20/23
  */
 
-/**
- * Initializes new NPC
- * @param {Object} props         NPC position and display properties
- * @param {Image} props.sprite   NPC sprite sheet
- * @param {number} props.x       X position on canvas to draw NPC sprite
- * @param {number} props.y       Y position on canvas to draw NPC sprite
- * @param {number} props.sWidth  Width of NPC sprite on sprite sheet
- * @param {number} props.sHeight Height of NPC sprite on sprite sheet
- * @param {number} props.scale   Scale factor to apply to NPC sprite, 1 by default
- * @returns {Object}             The NPC properties.
- * @constructor
- */
-const NPC = function(props) {
-    return {
-        tag: 'npc',
-        components: [
+class NPC {
+
+    /**
+     * Initializes new NPC
+     * @param {Object} props         NPC position and display properties
+     * @param {Image} props.sprite   NPC sprite sheet
+     * @param {number} props.x       X position on canvas to draw NPC sprite
+     * @param {number} props.y       Y position on canvas to draw NPC sprite
+     * @param {number} props.sWidth  Width of NPC sprite on sprite sheet
+     * @param {number} props.sHeight Height of NPC sprite on sprite sheet
+     * @param {number} props.scale   Scale factor to apply to NPC sprite, 1 by default
+     * @param {number} props.padding Pixel padding to apply to NPC sprite, 0 by default
+     * @param {number} props.fps     Frames per second of NPC sprite animation, 30 by default
+     * @returns {Object}             The NPC properties.
+     * @constructor
+     */
+    constructor(props) {
+        this.tag = 'npc';
+        this.name = 'npc';
+        this.components = [
             new CSprite({
                 sprite: props.sprite,
                 sWidth: props.sWidth,
                 sHeight: props.sHeight,
                 scale: props.scale,
-                fps: 30
+                padding: props.padding,
+                fps: props.fps || 30
             }),
             new CTransform({
                 x: props.x,
@@ -40,9 +45,18 @@ const NPC = function(props) {
             }),
             new CRigidBody(),
             // new CInput(),
-            new CState()
-        ]
+            // new CState()
+        ];
+        this.#addAnimations();
+        this.components.find(component => component.name === 'transform').register(
+            this.components.find(component => component.name === 'boxCollider'));
+    };
+
+    #addAnimations() {
+        const aMap = this.components.find(component => component.name === 'sprite').animationMap;
+        aMap.set('idleR', new AnimationProps(0, 0));
+        aMap.set('idleL', new AnimationProps(1, 0));
+        aMap.set('walkR', new AnimationProps(0, 1, 11));
+        aMap.set('walkL', new AnimationProps(0, 2, 11));
     };
 }
-NPC.prototype.name = 'npc';
-
