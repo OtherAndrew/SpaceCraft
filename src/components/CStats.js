9c9f172d479d3debd9af666f371f3ae41a1867ce
Andrew Nguyen <andrewnguyen841@gmail.com>
2023-02-09 14:06:42 -0800

    add methods to CStats.js for damage/defense

--- src/components/CStats.js
+++ src/components/CStats.js
@@ -1,6 +1,6 @@
 class CStats {
-    constructor({damage = 0, maxHealth = 1, speed = 0 }) {
-        Object.assign(this, { damage, maxHealth, speed })
+    constructor({damage = 0, maxHealth = 1, speed = 0, invincible = false }) {
+        Object.assign(this, { damage, maxHealth, speed, invincible })
         this.currentHealth = this.maxHealth;
         this.defenseMod = 1;
         this.damageMod = 1;
@@ -9,18 +9,36 @@ class CStats {
     }
 
     applyDamage(damage) {
-        this.currentHealth -= damage * this.defenseMod;
+        if (!this.invincible) {
+            this.currentHealth -= damage * this.defenseMod;
+        }
+    }
+
+    heal(amount) {
+        this.currentHealth = clamp(this.currentHealth + amount, 0, this.maxHealth);
     }
 
     doDamage() {
         return this.damage * this.damageMod;
     }
 
+    /**
+     * Add damage resistance.
+     * @param {number} modifier Percentage between 1 and 0 (30% damage resist = 0.30)
+     */
     addDefense(modifier) {
         this.defenseMod -= modifier;
     }
 
-    addAttack(modifier) {
+    /**
+     * Add attack buff.
+     * @param {number} modifier Percentage between 1 and 0 (30% damage buff = 0.30)
+     */
+    addDamage(modifier) {
         this.damageMod += modifier;
+    }
+
+    addSpeed(modifier) {
+        this.speed *= (1 + modifier);
     }
 }
