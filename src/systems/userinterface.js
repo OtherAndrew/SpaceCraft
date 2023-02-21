6ac4da88aa171749955497f7cb9ec488883fe388
Anthony Nguyen <anguyenq@uw.edu>
2023-02-21 02:46:57 -0800

    Clean up weapon gauge code

--- src/systems/userinterface.js
+++ src/systems/userinterface.js
@@ -37,48 +37,23 @@ class HUD {
             ctx.rect(this.x, this.y, 42, 42);
             ctx.stroke();
             ctx.restore();
-        }
 
-        if (this.activeContainer.item && this.activeContainer.item.name &&
-            this.activeContainer.item.name.includes('weapon')) {
-            let weaponStats = this.activeContainer.item.components.weaponProps;
-
-            if (weaponStats.cooldownDuration > 1) {
-                // ctx.save();
-                // ctx.lineWidth = 2;
-                // ctx.beginPath();
-                // ctx.fillStyle = "black";
-                // ctx.rect(420, 670, 183, 10);
-                // ctx.fill();
-                // ctx.beginPath();
-                ctx.drawImage(ASSET_MANAGER.cache[MISC_PATH.BULLETFRAME], 462, 650, 100, 30)
-                let cooldownPercentage;
-                // = (weaponStats.cooldownTime === 0) ?
-                //     1 : weaponStats.cooldownTime / weaponStats.cooldownDuration;
-                if (weaponStats.cooldownTime === 0 && weaponStats.fireDuration === 0) {
-                    cooldownPercentage = 1;
-                } else if (weaponStats.fireTime >= weaponStats.fireDuration) {
-                    cooldownPercentage = Math.max(0,
-                        weaponStats.cooldownTime / weaponStats.cooldownDuration);
-                } else {
-                    cooldownPercentage = 1 - Math.max(0,
-                        weaponStats.fireTime / weaponStats.fireDuration);
+            let active = this.activeContainer.item;
+            if (active && active.name && active.name.includes('weapon')) {
+                let weaponStats = active.components.weaponProps;
+                if (weaponStats.cooldownDuration > 1) {
+                    ctx.drawImage(ASSET_MANAGER.cache[MISC_PATH.BULLETFRAME], 462, 650, 100, 30)
+                    let cooldownPercentage;
+                    if (weaponStats.cooldownTime === 0 && weaponStats.fireDuration === 0) {
+                        cooldownPercentage = 1;
+                    } else if (weaponStats.fireTime >= weaponStats.fireDuration) {
+                        cooldownPercentage = weaponStats.cooldownTime / weaponStats.cooldownDuration;
+                    } else {
+                        cooldownPercentage = 1 - weaponStats.fireTime / weaponStats.fireDuration;
+                    }
+                    ctx.drawImage(ASSET_MANAGER.cache[MISC_PATH.BULLET], 0, 0, 495 * cooldownPercentage, 207, 462, 650,
+                        100 * cooldownPercentage, 30)
                 }
-
-                // let cooldownPercentage = (weaponStats.cooldownDuration - weaponStats.cooldownTime) / weaponStats.cooldownDuration;
-                // if (cooldownPercentage > 0.75) ctx.fillStyle = "green";
-                // else if (cooldownPercentage > 0.50) ctx.fillStyle = "yellow";
-                // else if (cooldownPercentage > 0.25) ctx.fillStyle = "orange";
-                // else ctx.fillStyle = "red";
-                ctx.drawImage(ASSET_MANAGER.cache[MISC_PATH.BULLET], 0, 0, 495 * cooldownPercentage, 207, 462, 650,
-                    100 * cooldownPercentage, 30)
-                // ctx.rect(420, 670, 183 * cooldownPercentage, 10); // depends on player health rep
-                // ctx.fill();
-                // ctx.beginPath();
-                // ctx.strokeStyle = "white";
-                // ctx.rect(420, 670, 183, 10);
-                // ctx.stroke();
-                // ctx.restore();
             }
         }
     }
