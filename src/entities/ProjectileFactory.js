0b9f23f36bd2056b7ae01777d39fa3bdbb786ef6
Andrew Nguyen <andrewnguyen841@gmail.com>
2023-02-23 03:58:05 -0800

    decrease railgun damage (1 shot to kill most things, 2 for wormtank)

--- src/entities/ProjectileFactory.js
+++ src/entities/ProjectileFactory.js
@@ -99,7 +99,7 @@ class ProjectileFactory {
                 projectileQueue.push(new Projectile({
                     tag: 'playerAttack ignoreTile pierce',
                     sprite: this.orbSprite(4),
-                    damage: 225,
+                    damage: 150,
                     speed: BLOCKSIZE * 0.9,
                     dVector: directionVector,
                     origin: projectileOrigin,
