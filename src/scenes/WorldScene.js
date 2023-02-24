22b2a4d14fa7307fcec2f7bfab721141c58eb620
Andrew Nguyen <andrewnguyen841@gmail.com>
2023-02-23 23:33:40 -0800

    comment out monster spawns

--- src/scenes/WorldScene.js
+++ src/scenes/WorldScene.js
@@ -64,16 +64,14 @@ class WorldScene extends Scene {
         const py = this.player.components['boxCollider'].center.y;
 
         // this.mobFactory.build('spikejumper', px + 300, py - 200);
-        this.mobFactory.build('bloodsucker', px - 300, py - 200);
+        // this.mobFactory.build('bloodsucker', px - 300, py - 200);
         // this.mobFactory.build('dirtcarver', px + 300, py - 200);
-        this.mobFactory.build('jawfly', px - 300, py - 200);
-        // this.mobFactory.build('wormtank', px - 300, py - 200);
-        // this.mobFactory.build('wormtank', px - 300, py - 200);
+        // this.mobFactory.build('jawfly', px - 300, py - 200);
         // this.mobFactory.build('wormtank', px - 300, py - 200);
         // this.mobFactory.build('mossfly', px - 300, py - 200);
-        this.mobFactory.build('silverfish', px + 600, py - 200);
+        // this.mobFactory.build('silverfish', px + 600, py - 200);
         // this.mobFactory.build('electrojelly', px - 600, py - 200);
-        this.mobFactory.build('bombfly', px + 900, py - 200);
+        // this.mobFactory.build('bombfly', px + 900, py - 200);
         // this.mobFactory.build('grapebomb', px - 900, py - 200);
         // this.mobFactory.build('spore', px + 1200, py - 200);
 
