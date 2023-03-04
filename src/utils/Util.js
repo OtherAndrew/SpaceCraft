c1cf824f2b125be6b60624954d99d64eef3cab51
Anthony Nguyen <anguyenq@uw.edu>
2023-03-03 16:46:16 -0800

    Simplify regenPlayerComponents

--- src/utils/Util.js
+++ src/utils/Util.js
@@ -370,59 +370,10 @@ const blur = (imageData, radius, quality) => {
 }
 
 const regenPlayerComponents = (props, currentPlayer) => {
-    const stats = new CStats({
-        maxHealth: 100,
-        speed: 5,
-        // regenCooldown: 10,
-        regenAmount: 0.1,
-        // invincible: true
-    });
-    const spriteWidth = 200;
-    const sprite = new CSprite({
-        sprite: ASSET_MANAGER.getAsset(CHAR_PATH.PLAYER),
-        sWidth: spriteWidth,
-        sHeight: 250,
-        scale: BLOCKSIZE * 1.5 / spriteWidth,
-        fps: 30
-    });
-    const transform = new CTransform({
-        x: props.x,
-        y: props.y,
-        hasGravity: true,
-        maxVelocityX: stats.speed
-    });
-    const cWidth = BLOCKSIZE * .8;
-    const collider = new CBoxCollider({
-        x: props.x,
-        y: props.y,
-        width: cWidth,
-        height: BLOCKSIZE * 1.35,
-        xOffset: (sprite.dWidth - cWidth) / 2,
-        yOffset: BLOCKSIZE * .4
-    });
-
-    const aMap = sprite.animationMap;
-    aMap.set('idleR', new AnimationProps(0, 0));
-    aMap.set('idleL', new AnimationProps(1, 0));
-    aMap.set('walkR', new AnimationProps(0, 1, 11));
-    aMap.set('walkL', new AnimationProps(0, 2, 11));
-    aMap.set('jumpR', new AnimationProps(0, 1));
-    aMap.set('jumpL', new AnimationProps(0, 2));
-    aMap.set('flyR', new AnimationProps(0, 1));
-    aMap.set('flyL', new AnimationProps(0, 2));
-    aMap.set('crouchR', new AnimationProps(5, 1));
-    aMap.set('crouchL', new AnimationProps(5, 2));
-    
-    transform.collider = collider
-    const state = new CState();
-    state.sprite = sprite;
-
-    let newComponents = [stats, sprite, transform, collider, state];
-    
+    let newPlayer = new Player(props);
     currentPlayer.isDrawable = true;
     currentPlayer.isAlive = true;
     currentPlayer.components = {};
-    
-    newComponents.forEach(c => currentPlayer.components[c.name] = c)
+    newPlayer.components.forEach(c => currentPlayer.components[c.name] = c)
 }
 
