0b2b7a2573eeca2ad6aef33889e0225d403ca1b0
Andrew Nguyen <andrewnguyen841@gmail.com>
2023-03-08 12:54:28 -0800

    add new item icons

--- src/constants.js
+++ src/constants.js
@@ -134,9 +134,9 @@ const ITEM_PATH = {
 
     AMBER: './assets/items/amber.png',
     KERATIN: './assets/items/mob_dirtCarver.png',
-    WORMTANK: './assets/items/mob_wormTank.png',
-    STICKY: './assets/items/mob_stickySlime.png',
-    SPIDER: './assets/items/spiderSilk.png',
+    WORMTANK: './assets/items/mob_wormTank2.png',
+    STICKY: './assets/items/mob_stickySlime2.png',
+    SPIDER: './assets/items/spiderSilk2.png',
 
     CIRCUIT: './assets/items/circuit.png',
     SMART: './assets/items/smartCircuit.png',
